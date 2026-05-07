75b93227e051bd9e669597f0ab33c5a18f098939
Reema Mohammed |  ريما بنت محمد <reemozezo@gmail.com>
2026-05-07 16:39:17 +0300

    Create main.java

---
+++ Stuck/main.java
@@ -0,0 +1,18 @@
+public class main {
+    public static void main(String[] args) {
+        
+        ArrayStack<String> reema = new ArrayStack<String>();
+        reema.push("p1");
+        reema.push("p2");
+        reema.push("p3");
+        reema.push("p4");
+        reema.push("p5");
+        
+        /*
+        printer p = new printer();
+        
+        p.addPaper(10);
+        p.printDocument();
+        */
+    }
+}
